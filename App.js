be534fdcff66496fdc36170c93ecb893533bb736
kushar77 <kusharora339@gmail.com>
2022-05-07 23:46:06 -0700

    Connected to backend

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const ipAddr = "192.168.0.16"; //Find and enter the IP address of your computer!
+  const [ users, setUsers ] = useState([]);
+  const [ reqData, setReqData ] = useState(false);
+
+  useEffect(() => {
+    if(reqData){
+      fetch(`http://${ipAddr}:3000/users`)
+      .then(res => res.json())
+      .then((result) => {
+        setUsers(result);
+      })
+    }
+  }, [reqData]);
+
   return (
     <View style={styles.container}>
-      <Text> KUSH is not straight </Text>
+      <Button 
+        onPress={() => setReqData(true)}
+        title="Request data"
+      />
+      <Text style={{paddingTop: 25}}>backend data:</Text>
+      {users.map((data, i) => (
+        <Text key={i}>{data.name}</Text>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
